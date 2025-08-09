6aea49641ea5127d7bb04268ea29fcbfad2591fb
olawhales <ajaditaoreed@gmail.com>
2025-08-09 12:27:48 +0100

    Addition of admin and validation exception classes

---
+++ src/main/java/com/semicolon/ecommerceTask/domain/exception/AdminException.java
@@ -0,0 +1,7 @@
+package com.semicolon.ecommerceTask.domain.exception;
+
+public class AdminException extends RuntimeException {
+    public AdminException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/semicolon/ecommerceTask/domain/model/AdminDomainObject.java
+++ src/main/java/com/semicolon/ecommerceTask/domain/model/AdminDomainObject.java
@@ -1,4 +1,20 @@
 package com.semicolon.ecommerceTask.domain.model;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Builder
 public class AdminDomainObject {
+    private UUID id;
+    private String keycloakId;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private List<String> roles; // e.g., ["ADMIN"]
 }
+
