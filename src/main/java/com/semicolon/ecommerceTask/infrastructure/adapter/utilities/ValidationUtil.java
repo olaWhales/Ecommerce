d09878f1fbec7e4fb9c8ddebf902a3c0ef5f9f26
olawhales <ajaditaoreed@gmail.com>
2025-08-11 00:02:48 +0100

    Addition of seller registration entity

---
+++ src/main/java/com/semicolon/ecommerceTask/infrastructure/adapter/utilities/ValidationUtil.java
@@ -0,0 +1,23 @@
+package com.semicolon.ecommerceTask.infrastructure.adapter.utilities;
+
+import com.semicolon.ecommerceTask.domain.exception.ValidationException;
+
+import java.util.regex.Pattern;
+
+public class ValidationUtil {
+
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
+    private static final Pattern PASSWORD_PATTERN = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*?])(?=\\S+$).{8,}$");
+
+    public static void validateEmail(String email) {
+        if (email == null || email.trim().isEmpty() || !EMAIL_PATTERN.matcher(email).matches()) {
+            throw new ValidationException(MessageUtil.INVALID_EMAIL);
+        }
+    }
+
+    public static void validatePassword(String password) {
+        if (password == null || !PASSWORD_PATTERN.matcher(password).matches()) {
+            throw new ValidationException(MessageUtil.INVALID_PASSWORD);
+        }
+    }
+}
